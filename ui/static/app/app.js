484fd7d37a6e9ad7b356c38d187d518a9a6324e4
jjohxx <johxgks@gmail.com>
2016-07-29 10:24:48 -0400

    Gameroom waiting basic uploaded

--- ui/static/app/app.js
+++ ui/static/app/app.js
@@ -1,18 +1,61 @@
-angular.module("mainModule", [])
-  .controller("mainController", function ($scope)
-  {
+angular.module("mainModule", ['ui.router'])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    // For any unmatched url, send to /game-room
+    $urlRouterProvider.otherwise("/game-room")
+
+    $stateProvider
+      .state('game-room', {
+        url: "/game-room",
+        templateUrl: "views/game-room.html",
+        controller: function ($scope) {
+          $scope.gameroom = {
+            id: 1,
+            name: 'Battle Blood',
+            numberPlayers: 8
+          };
+          $scope.players = [{
+            id: 1,
+            username: 'VaniMar',
+            user: 'user'
+          }, {
+              id: 2,
+              username: 'JOhx',
+              user: 'user'
+            }, {
+              id: 3,
+              username: '',
+              user: 'playerdisabled'
+            }, {
+              id: 4,
+              username: '',
+              user: 'playerdisabled'
+            }, {
+              id: 5,
+              username: '',
+              user: 'playerdisabled'
+            }, {
+              id: 6,
+              username: '',
+              user: 'playerdisabled'
+            }, {
+              id: 6,
+              username: '',
+              user: 'playerdisabled'
+            }];
+        }
+      })
+  })
+  .controller("mainController", function ($scope) {
     $scope.myScopeVar = "the scope variable value";
   })
   // 1. Directive with a template specified inline
-  .directive("nghTemplateDir", function ()
-  {
+  .directive("nghTemplateDir", function () {
     return {
       template: 'This is <strong>nghTemplateDir</strong> directive printing <em>{{myScopeVar}}</em>'
     };
   })
   // 2. Directive with a template loaded from a URL
-  .directive("nghTemplateUrlDir", function ()
-  {
+  .directive("nghTemplateUrlDir", function () {
     return {
       templateUrl: 'templates/nghTemplateUrlDirBody.html'
     };
