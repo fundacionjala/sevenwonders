b88e6ca346adbab885fb54bb5eff3881e286ebc4
jjohxx <johxgks@gmail.com>
2016-08-09 19:49:15 -0600

    Fixed unused code

--- ui/static/app/module/directives/game-room/game-room.html.js
+++ ui/static/app/module/directives/game-room/game-room.html.js
@@ -6,12 +6,6 @@ angular.module("sevenWonder")
 				gameroom: '='
 			},
 			replace: true,
-			templateUrl: 'app/module/directives/game-room/game-room.html',
-			controller: function ($scope) {
-				$scope.count = 0;
-				$scope.setCount = function (val) {
-					$scope.count = val;
-				}
-			}
+			templateUrl: 'app/module/directives/game-room/game-room.html'
 		}
 	});
