6c8c926f2d9812987900117f1c51845cffa2cbaf
jjohxx <johxgks@gmail.com>
2016-08-22 23:51:32 -0400

    Folders deleted without use and refactoring branch updated of my origin

--- ui/static/app/module/services/game-room/swGameRooms.js
+++
@@ -1,6 +0,0 @@
-angular.module("sevenWonder")
-  .service('swGameRooms', function ($http) {
-    this.getGameRoom = function () {
-      return $http.get('http://demo9730175.mockable.io/game-room/7');
-    }
-  });
