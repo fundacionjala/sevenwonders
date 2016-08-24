abe4d3b5122f0f2ff027599966eb0bce2eeefdb1
Diego Fiengo Arnez <diego.fiengo.arnez@hotmail.com>
2016-08-24 09:23:20 -0400

    Clean code and fixied it (#39)

--- ui/src/main/javascript/app/gameroom/gameroom.component.js
+++ ui/src/main/javascript/app/gameroom/gameroom.component.js
@@ -11,57 +11,46 @@ angular.
                 //     self.games = result.games;
                 // });
                 this.gameroom = {
-                    "id": 7,
-                    "name": "Stalingrado",
-                    "numberPlayers": 7,
-                    "players": [{
-                        "id": 1,
-                        "username": "Diego",
-                        "avatar": "user"
+                    'id': 7,
+                    'name': 'Stalingrado',
+                    'numberPlayers': 7,
+                    'players': [{
+                        'id': 1,
+                        'username': 'Diego',
+                        'avatar': 'user'
                     }, {
-                            "id": 2,
-                            "username": "JOhx",
-                            "avatar": "user"
+                            'id': 2,
+                            'username': 'JOhx',
+                            'avatar': 'user'
                         }, {
-                            "id": 3,
-                            "username": "Gumu",
-                            "avatar": "user"
+                            'id': 3,
+                            'username': 'Gumu',
+                            'avatar': 'user'
                         }, {
-                            "id": 4,
-                            "username": "",
-                            "avatar": ""
+                            'id': 4,
+                            'username': '',
+                            'avatar': ''
                         }, {
-                            "id": 5,
-                            "username": "",
-                            "avatar": ""
+                            'id': 5,
+                            'username': '',
+                            'avatar': ''
                         }, {
-                            "id": 6,
-                            "username": "",
-                            "avatar": ""
+                            'id': 6,
+                            'username': '',
+                            'avatar': ''
                         }, {
-                            "id": 7,
-                            "username": "",
-                            "avatar": ""
+                            'id': 7,
+                            'username': '',
+                            'avatar': ''
                         }]
-
-<<<<<<< HEAD
-                }
+                };
                 this.maxPlayers = 7;
                 this.createGame = function () {
 
-                }
+                };
                 this.joinGame = function () {
 
-                }
+                };
             }
         ]
     });
-=======
-            };
-            this.joinGame = function() {
-
-            };
-        }
-    ]
-});
->>>>>>> fc77ff7b8b69d4b1b6390cb591ab4e7b476cc980
