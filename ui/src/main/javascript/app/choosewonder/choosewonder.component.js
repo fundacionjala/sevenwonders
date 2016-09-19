e2413edf81abbed84d0332e96653f84db589f837
jjohxx <johxgks@gmail.com>
2016-09-19 08:07:30 -0400

    Functionality rotate added

--- ui/src/main/javascript/app/choosewonder/choosewonder.component.js
+++ ui/src/main/javascript/app/choosewonder/choosewonder.component.js
@@ -6,37 +6,19 @@ angular.
         templateUrl: 'choosewonder/choosewonder.tpl.html',
         controller: ['ChooseWonder',
             function ChooseWonderController(ChooseWonder) {
-
-              var self = this;
-              self.wonderPlayers= [];
-              ChooseWonder.getWonderPlayers().then(function (result) {
-                       self.wonderPlayers = result;
-                       console.log(self.wonders[0]);
-              });
-
-                var listOfWonders = [{
-                    'name': "olympia"
-                }, {
-                        'name': "babylon"
-                    }, {
-                        'name': "ephesos"
-                    }, {
-                        'name': "alexandria"
-                    }, {
-                        'name': "gizah"
-                    }, {
-                        'name': "halinarkassus"
-                    }, {
-                        'name': "rodhos"
-                    }];
-                self.wonders = listOfWonders;
+                var self = this;
+                self.wonderPlayers = [];
+                ChooseWonder.getWonderPlayers().then(function (result) {
+                    self.wonderPlayers = result;
+                    self.wonders = self.wonderPlayers;
+                });
                 this.rotate = function (data) {
                     if (data == 'next') {
-                        listOfWonders.slice(0, 3);
-                        self.wonders.push(listOfWonders.shift());
+                        self.wonderPlayers.slice(0, 3);
+                        self.wonders.push(self.wonderPlayers.shift());
                     } else if (data == 'previous') {
-                        listOfWonders.slice(0, 3);
-                        self.wonders.unshift(listOfWonders.pop());
+                        self.wonderPlayers.slice(0, 3);
+                        self.wonders.unshift(self.wonderPlayers.pop());
                     }
                 };
             }
