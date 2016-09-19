c4c9be8ce68b5b377a9bbac08187f93c85938b30
Jhonatan Mamani <johxgks@gmail.com>
2016-09-19 08:20:37 -0400

    View of the wonders implemented (#59)
    * define conditional for showing button create game room
    
    * create a test POST for createGameRoom
    
    * remove spaces in create-game-room.html
    
    * remove spaces of tags in create-game-room.html
    
    * Adapting the view game room based on what we learned as to be less, architecture for applying best practices
    
    * Deleted bootstrap of the index and fixed create game room
    
    * Fixed unused code
    
    * Folders deleted without use and refactoring branch updated of my origin
    
    * Added the foundation Jala proxies to .bowerrc and .npmrc
    
    * Removed file gameRoomCtrl.js without use
    
    * Added dependency and import of angular-websocket
    
    * Removed proxies in .npmrc
    
    * Built by (gradle gulp_buildIndex) command, import of angular-websocket to index.html
    
    * Added gameroom services consume client side Johx
    
    * GameRoom implementation added Jhox
    
    * Basic structure for view choosewonder created
    
    * Style  for view choosewonder created
    
    * Style basic choosewonder updated
    
    * Carousel of three views for wonders implemented
    
    * Functionality rotate added
    
    * Attribute name of wonder renamed

--- ui/src/main/javascript/app/choosewonder/choosewonder.component.js
+++ ui/src/main/javascript/app/choosewonder/choosewonder.component.js
@@ -6,7 +6,32 @@ angular.
         templateUrl: 'choosewonder/choosewonder.tpl.html',
         controller: ['ChooseWonder',
             function ChooseWonderController(ChooseWonder) {
-                
+                var self = this;
+                var listOfWonders = [{
+                    'name': "olympia"
+                }, {
+                        'name': "babylon"
+                    }, {
+                        'name': "ephesos"
+                    }, {
+                        'name': "alexandria"
+                    }, {
+                        'name': "gizah"
+                    }, {
+                        'name': "halinarkassus"
+                    }, {
+                        'name': "rodhos"
+                    }];
+                self.wonders = listOfWonders;
+                this.rotate = function (data) {
+                    if (data == 'next') {
+                        listOfWonders.slice(0, 3);
+                        self.wonders.push(listOfWonders.shift());
+                    } else if (data == 'previous') {
+                        listOfWonders.slice(0, 3);
+                        self.wonders.unshift(listOfWonders.pop());
+                    }
+                };
             }
         ]
     });
