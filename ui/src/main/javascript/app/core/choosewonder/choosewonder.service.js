d815c821f4748044a87b19ee00aa43deb5478fb7
Juan Manuel Barahona <dwits@outlook.com>
2016-10-10 08:18:10 -0400

    Integrate choose wonder (#66)
    * add wonder change side
    
    * Add button and function for  ready
    
    * Implement the part of the users in the chooseWonder and select a user
    
    * Add service for choosewonder
    
    * Add integration with service
    
    * fix service

--- ui/src/main/javascript/app/core/choosewonder/choosewonder.service.js
+++ ui/src/main/javascript/app/core/choosewonder/choosewonder.service.js
@@ -5,6 +5,17 @@ angular.
     factory('ChooseWonder', ['$cookies', '$websocket', 'Game', 'Restangular', '$q', '$location',
         function ($cookies, $websocket, Game, Restangular, $q, $location) {
                 return {
+                    setWonderPlayer : function(player){
+                        return $q(function(resolve, reject){
+                        Restangular.all('games/' + Game.getCurrentGame().id + '/player').customPUT(player)
+                                   .then(function(data) {
+                                         resolve(data);
+                                   }).catch(function(data){
+                                         reject(data);
+                                   });
+                       });
+                    },
+
                     getWonderPlayers : function () {
                        return $q(function(resolve, reject){
                         Restangular.one('games', Game.getCurrentGame().id).getList('players')
