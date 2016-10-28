38ff972be1aac4db5b7ca2a95e4e671595bcda3d
Dwits <dwits@outlook.com>
2016-10-28 07:47:49 -0400

    Add chooseWonder and gameroom tests

--- ui/src/main/javascript/app/core/gameroom/gameroom.service.spec.js
+++ ui/src/main/javascript/app/core/gameroom/gameroom.service.spec.js
@@ -2,12 +2,18 @@
 describe('getGameRoom', function(){
     var gameRoom;
     var game;
+    var httpBackend;
+    var restangular;
+    var websocket;
 
     beforeEach(function() {
         module('sevenWonders.core.gameroom');
-        inject(function($injector){
+        inject(function($injector, _$httpBackend_){
+            websocket = $injector.get('$websocket');
             gameRoom = $injector.get('GameRoom');
             game = $injector.get('Game');
+            httpBackend = _$httpBackend_;
+            restangular = $injector.get('Restangular');
             spyOn(game, 'getCurrentGame').and.returnValue({
                     id: 1,
                     roomName: "Tu",
@@ -38,7 +44,39 @@ describe('getGameRoom', function(){
 
     
     it('should return the list of players', () => {
-        
+
+        var gameModel = {
+            id: 1
+        };
+     
+        var mockPlayers = [
+                {
+                name:"juan",
+                token:"asdasdasd"
+                },
+                {
+                name:"dwtis",
+                token:"asd"
+                }
+            ];
+
+        spyOn(restangular, 'one').and.callThrough();
+
+        httpBackend.expectGET('/games/1/players').respond(mockPlayers);
+
+        var players = gameRoom.getPlayers();
+        expect(restangular.one).toHaveBeenCalledWith('games', 1);
+        httpBackend.flush();    
+
+        players.then(function(value){
+            expect(value).toEqual(mockPlayers);
+        });
     });
+
+    
+    it('should addPlayer onMessage', () => {
+       
+    });
+        
          
 });
