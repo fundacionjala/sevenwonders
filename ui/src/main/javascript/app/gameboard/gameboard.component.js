75cd2a46aa9153e570e2b6468b18f18451ccfeae
VaniMar <vania.catorceno@fundacion-jala.org>
2016-11-09 05:38:12 -0600

    Draw game board (#91)
    * drawn main base for gameboard view
    
    * implement mock for get storage information
    
    * drawn in the header the resource of the storage
    
    * added new icons for scientific cards
    
    * modified less and component of the game board
    
    * modified details for game board tpl adding, rename some icons
    
    * rename some images
    
    * modified the call url mock, fixed a bug in ui gameboard
    
    * modified details in less the gameboard, rename the tokens

--- ui/src/main/javascript/app/gameboard/gameboard.component.js
+++ ui/src/main/javascript/app/gameboard/gameboard.component.js
@@ -3,5 +3,16 @@
 angular.
     module('sevenWonder.gameboard').
     component('gameboard', {
-        templateUrl: 'gameboard/gameboard.tpl.html'
+        templateUrl: 'gameboard/gameboard.tpl.html',
+        controller: ['GameBoard',
+            function GameBoardController(GameBoard){
+                var self = this;
+                self.storage = [];
+                GameBoard.getStorage().then(function (result) {
+                                        result.forEach(function (element) {
+                                                self.storage.push(element);
+                                              }, this);
+                                      });
+            }
+        ]
     });
