c501d03d04f9da5da5df4385599ce97dc21483db
Dwits <dwits@outlook.com>
2016-11-16 10:30:00 -0400

    Add integration with backend

--- ui/src/main/javascript/app/core/gameboard/gameboard.service.js
+++ ui/src/main/javascript/app/core/gameboard/gameboard.service.js
@@ -14,7 +14,33 @@ angular.
                                        defer.reject();
                                  });
                          return defer.promise;
-                   }
-                }
+                 },
+                 getPlayerCards: function() {
+                     var principalGame = $cookies.getObject('PrincipalGame');
+                     var loggedUser = $cookies.getObject('user');
+                     var cards = [];
+                     var defer = $q.defer();
+                     Restangular.one('game', principalGame.id).getList('players')
+                                .then(function (data) {
+                                    var currentPlayer;
+                                    data.forEach(function(data){
+                                        if(data.userName == loggedUser.userName){
+                                            currentPlayer = data;
+                                            return;
+                                        }
+                                    });
+                
+                                    currentPlayer.deck.cards.forEach(function(data){
+                                        cards.push(data.name);
+                                    })
+
+                                    defer.resolve(cards);
+
+                                }).catch(function () {
+                                    defer.reject();
+                                });     
+                    return defer.promise;
+                 }
+               }
         }
     ]);
